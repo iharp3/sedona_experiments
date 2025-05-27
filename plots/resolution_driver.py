b4e2bf5a0f188304f9eef58a898548d7711e4488
ana-e-uk <uribe055@umn.edu>
2025-05-27 17:20:38 -0500

    order t resolution hdmy

--- plots/resolution_driver.py
+++ plots/resolution_driver.py
@@ -6,6 +6,8 @@ Edited from github repo:
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
+from pandas.api.types import CategoricalDtype
+import numpy as np
 
 # Load the CSV file
 csv_file_path = "/home/uribe055/sedona_experiments/results/all_resolutions.csv"
@@ -16,17 +18,18 @@ line = "sys"
 y = "total_time"
 
 # Get unique plot values
-cur_plot = "t_res"
-unique_plots = ["hour", "day", "year"]
-tick_list = [0.25, 0.5, 1.0]
-x = "s_res"
-x_label = "Spatial resolution (degrees)"
+# cur_plot = "t_res"
+# unique_plots = ["hour", "day", "year"]
+# tick_list = [0.25, 0.5, 1.0]
+# x = "s_res"
+# x_label = "Spatial resolution (degrees)"
 
-# cur_plot = "s_res"
-# unique_plots = [0.5, 1.0]
-# tick_list = ["hour", "day", "month", "year"]
-# x = "t_res"
-# x_label = "Temporal resolution"
+cur_plot = "s_res"
+unique_plots = [0.5, 1.0]
+tick_list = ["hour", "day", "month", "year"]
+tick_labels = ["hour", "day", "month", "year"]
+x = "t_res"
+x_label = "Temporal resolution"
 
 marker_size = 25
 m_fill = "none"
@@ -34,7 +37,6 @@ font_size = 30
 tick_font_size = 30
 tick_size = 30
 
-tick_labels = tick_list
 line_width = 4
 above = "bottom"
 below = "top"
@@ -52,7 +54,7 @@ style_dict = {
 
 # Determine global y-axis limits
 y_min = df[y].min()
-y_max = df[y].max()
+y_max = np.power(10, np.log10(df[y].max()) + 0.1)
 
 legend_position = ["best", "lower left", "best", "center"]    # 'best', 'upper right', 'upper left', 'lower left', 'lower right', 'right', 'center left', 'center right', 'lower center', 'upper center', 'center'
 
@@ -63,10 +65,9 @@ for plot_value, position  in zip(unique_plots, legend_position):
     
     for line_value in subset[line].unique():
         line_data = subset[subset[line] == line_value]  # df[df[system] == polaris]
+        cat_type = CategoricalDtype(categories=tick_list, ordered=True)
+        line_data[x] = line_data[x].astype(cat_type)
 
-        # if line_value == "Polaris":
-        #     line_data = line_data.groupby(x, as_index=False)["tr"].mean()   # Average over tr values
-        # else:
         line_data = line_data.groupby(x, as_index=False)[y].mean()  # Average over x values
         
         line_data = line_data.sort_values(by=x)  # Ensure lines are connected correctly
@@ -74,18 +75,14 @@ for plot_value, position  in zip(unique_plots, legend_position):
         # Get style properties from dictionary, use defaults if not found
         style = style_dict.get(line_value, {"marker": "o", "markersize": 4, "linewidth": 1.5, "color": "black", "labelsize": 10, "ticksize": 8})
         
-        # if line_value == "Polaris":
-        #     ax.plot(line_data[x], line_data["tr"], 
-        #             marker=style["marker"], markersize=style["markersize"], fillstyle=m_fill,
-        #             linewidth=style["linewidth"], color=style["color"], label=f"{line_value}")
-        # else:
         ax.plot(line_data[x], line_data[y], 
                 marker=style["marker"], markersize=style["markersize"], fillstyle=style["fill"],
                 linewidth=style["linewidth"], color=style["color"], label=f"{line_value}")
     
     ax.set_xlabel(x_label, fontsize=font_size)
     if style["ticklist"] is not None:
-        ax.set_xticks(ticks=style["ticklist"], labels=style["ticklabels"])
+        ax.set_xticks(ticks=style["ticklist"])
+        ax.set_xticklabels(style["ticklabels"])
     ax.set_ylabel(y_label, fontsize=font_size)
     ax.set_yscale("log")  # Set y-axis to log scale
     ax.set_ylim(y_min, y_max)
@@ -93,7 +90,7 @@ for plot_value, position  in zip(unique_plots, legend_position):
         # ax.legend(fontsize=font_size-10, loc=position, bbox_to_anchor=(0.37,0.53))   # for datasets_separate
         ax.legend(fontsize=font_size-7, loc="best")   # for datasets_averaged
     elif plot_value == 0.5 or plot_value == 1.0:
-        ax.legend(fontsize=font_size-7, loc="best") 
+        ax.legend(fontsize=font_size-8, loc="best") 
     else:
         # ax.legend(fontsize=font_size-10, loc=position)     # for datasets_separate
         ax.legend(fontsize=font_size, loc=position)     # for datasets_averaged
